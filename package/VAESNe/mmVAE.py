9dffcd4001bc22e9f6a0f8f71f19b659da3adcad
yshen99 <yshen99@orcd-login001.mit.edu>
2025-05-11 18:20:03 -0400

    minor change on multimodal

--- package/VAESNe/mmVAE.py
+++ package/VAESNe/mmVAE.py
@@ -15,10 +15,10 @@ from util_layers import get_mean, log_mean_exp, kl_divergence
 ### base class of mmVAE
 
 class MMVAE(nn.Module):
-    def __init__(self, prior_dist, params, *vaes):
+    def __init__(self, prior_dist, vaes):
         super(MMVAE, self).__init__()
         self.pz = prior_dist
-        self.vaes = nn.ModuleList([vae(params) for vae in vaes])
+        self.vaes = nn.ModuleList(vaes)
         self.modelName = None  # filled-in per sub-class
         self.params = params
         self._pz_params = None  # defined in subclass
@@ -65,39 +65,4 @@ class MMVAE(nn.Module):
             # cross-modal matrix of reconstructions
             recons = [[get_mean(px_z) for px_z in r] for r in px_zs]
         return recons
-
-
-def compute_microbatch_split(x, K):
-    """ Checks if batch needs to be broken down further to fit in memory. """
-    B = x[0].size(0) if is_multidata(x) else x.size(0)
-    S = sum([1.0 / (K * prod(_x.size()[1:])) for _x in x]) if is_multidata(x) \
-        else 1.0 / (K * prod(x.size()[1:]))
-    S = int(1e8 * S)  # float heuristic for 12Gb cuda memory
-    assert (S > 0), "Cannot fit individual data in memory, consider smaller K"
-    return min(B, S)
-
-def _m_iwae(model, x, K=1):
-    """IWAE estimate for log p_\theta(x) for multi-modal vae -- fully vectorised"""
-    qz_xs, px_zs, zss = model(x, K)
-    lws = []
-    for r, qz_x in enumerate(qz_xs):
-        lpz = model.pz(*model.pz_params).log_prob(zss[r]).sum(-1)
-        lqz_x = log_mean_exp(torch.stack([qz_x.log_prob(zss[r]).sum(-1) for qz_x in qz_xs]))
-        lpx_z = [px_z.log_prob(x[d]).view(*px_z.batch_shape[:2], -1)
-                     .mul(model.vaes[d].llik_scaling).sum(-1)
-                 for d, px_z in enumerate(px_zs[r])]
-        lpx_z = torch.stack(lpx_z).sum(0)
-        lw = lpz + lpx_z - lqz_x
-        lws.append(lw)
-    return torch.cat(lws)  # (n_modality * n_samples) x batch_size, batch_size
-
-
-def m_iwae(model, x, K=1):
-    """Computes iwae estimate for log p_\theta(x) for multi-modal vae """
-    S = compute_microbatch_split(x, K)
-    x_split = zip(*[_x.split(S) for _x in x])
-    lw = [_m_iwae(model, _x, K) for _x in x_split]
-    lw = torch.cat(lw, 1)  # concat on batch
-    return log_mean_exp(lw).sum()
-
 

2adaaa3547a8c0e49173546abb2a099db9083a8f
Yunyi Shen <yshen99@mit.edu>
2025-05-25 15:41:56 -0400

    separate brightness and local structure

--- package/VAESNe/PhotometricVAE.py
+++ package/VAESNe/PhotometricVAE.py
@@ -5,6 +5,7 @@ from .PhotometricLayers import photometricTransformerEncoder, photometricTransfo
 from .base_vae import VAE
 import torch.distributions as dist
 from torch import nn
+from .util_layers import MLP
 
 class PhotometricEnc(nn.Module):
     def __init__(self, 
@@ -216,4 +217,139 @@ class PhotometricVAE(VAE):
             px_z = self.px_z(px_z_param)
             data = px_z.mean
         return data
+
+
+
+class BrightPhotometricVAE(VAE):
+    def __init__(self, photometric_length = 60,
+                num_bands = 6,
+                latent_len = 8,
+                latent_dim = 4,
+                model_dim = 64, 
+                num_heads = 4, 
+                ff_dim = 64, 
+                num_layers = 4,
+                dropout = 0.1,
+                selfattn = False,
+                beta = 1.,
+                prior = dist.Laplace,
+                likelihood = dist.Laplace,
+                posterior = dist.Laplace
+                ):
+        assert latent_len > 1, "first token for overall brightness"
+        super(BrightPhotometricVAE, self).__init__(
+            prior,  # prior
+            likelihood,  # likelihood
+            posterior,  # posterior
+            PhotometricEnc(num_bands,
+                latent_len,
+                latent_dim,
+                model_dim,
+                num_heads, 
+                ff_dim, 
+                num_layers,
+                dropout,
+                selfattn),
+            PhotometricDec(photometric_length,
+                 latent_dim,
+                num_bands,
+                model_dim, 
+                num_heads, 
+                ff_dim, 
+                num_layers,
+                dropout),
+            params = [photometric_length,
+                num_bands,
+                latent_len,
+                latent_dim,
+                model_dim, 
+                num_heads, 
+                ff_dim, 
+                num_layers,
+                dropout, selfattn]
+        )
+
+        self._pz_params = nn.ParameterList([
+            nn.Parameter(torch.zeros(latent_len, latent_dim), requires_grad=False),  # mu
+            nn.Parameter(torch.ones(latent_len, latent_dim), requires_grad=False)  # logvar
+        ])
+        self.llik_scaling = 1./beta
+        self.modelName = 'light_curve'
+        self.photometric_length = photometric_length
+        self.latent_len = latent_len
+        self.latent_dim = latent_dim
+        self.brightnessfc = MLP(latent_dim, 1, [model_dim])
+        #self.dataSize = dataSize
+    
+    def forward(self, x, K = 1):
+        flux, time, band, mask = x
+        self._qz_x_params = self.enc(flux, time, band, mask)
+        if torch.isnan(self._qz_x_params[0]).any() or torch.isnan(self._qz_x_params[1]).any():
+            breakpoint()
+        qz_x = self.qz_x(*self._qz_x_params)
+        zs = qz_x.rsample(torch.Size([K]))
+        '''
+        px_z_loc, px_z_scale = self.dec(time.unsqueeze(0).expand(K, -1, -1).view(-1, time.shape[-1]), 
+                                        band.unsqueeze(0).expand(K, -1, -1).view(-1, band.shape[-1]), 
+                                        zs.view(-1, zs.shape[-2], zs.shape[-1]), 
+                                        mask.unsqueeze(0).expand(K, -1, -1).view(-1, mask.shape[-1]))
+        
+        px_z_loc = px_z_loc.view(K, -1, flux.shape[-1])
+        px_z_scale = px_z_scale.view(K, -1, flux.shape[-1])
+
+        px_z = self.px_z(px_z_loc, px_z_scale)
+        '''
+        px_z = self.decode(zs, x)
+        return qz_x, px_z, zs
+    
+    
+    def encode(self, x, mean = True):
+        flux, time, band, mask = x
+        self.eval()
+        with torch.no_grad():
+            qz_x = self.qz_x(*self.enc(flux, time, band, mask))
+        if mean:
+            return qz_x.mean
+        return qz_x
+
+    def decode(self, zs, x):
+        _, time, band, mask = x
+        K = zs.shape[0]
+        brightness = zs[:, :,0, :]
+        brightness = self.brightnessfc(brightness)
+        zs = zs[:, :, 1:, :]
+        px_z_loc, px_z_scale = self.dec(time.unsqueeze(0).expand(K, -1, -1).reshape(-1, time.shape[-1]), 
+                                        band.unsqueeze(0).expand(K, -1, -1).reshape(-1, band.shape[-1]), 
+                                        zs.reshape(-1, zs.shape[-2], zs.shape[-1]), 
+                                        mask.unsqueeze(0).expand(K, -1, -1).reshape(-1, mask.shape[-1]))
+        
+        px_z_loc = px_z_loc.reshape(K, -1, self.photometric_length)
+        px_z_loc = px_z_loc - px_z_loc.mean(axis = 2)[:, :, None] + brightness
+        px_z_scale = px_z_scale.reshape(K, -1, self.photometric_length)
+        #breakpoint()
+        return self.px_z(px_z_loc, px_z_scale)
+
+    def reconstruct(self, x, K=1):
+        flux, time, band, mask = x
+        self.eval()
+        with torch.no_grad():
+            qz_x = self.qz_x(*self.enc(flux, time, band, mask))
+            zs = qz_x.rsample([K])  # no dim expansion
+            px_z = self.decode(zs, x)
+            recon = px_z.mean
+        return recon
+    
+    def generate(self, N, time, band, mask = None):
+        self.eval()
+        with torch.no_grad():
+            pz = self.pz(*self.pz_params)
+            zs = pz.rsample(torch.Size([N]))
+            px_z_param = self.dec(time.unsqueeze(0).expand(K, -1, -1).view(-1, time.shape[-1]), 
+                                        band.unsqueeze(0).expand(K, -1, -1).view(-1, band.shape[-1]), 
+                                        zs.view(-1, zs.shape[-2], zs.shape[-1]), 
+                                        mask.unsqueeze(0).expand(K, -1, -1).view(-1, mask.shape[-1]))
+            px_z = self.px_z(px_z_param)
+            data = px_z.mean
+        return data
+
 
--- package/VAESNe/SpectraVAE.py
+++ package/VAESNe/SpectraVAE.py
@@ -6,6 +6,7 @@ from .SpectraLayers import spectraTransformerDecoder, spectraTransformerEncoder
 
 from .base_vae import VAE
 import torch.distributions as dist
+from .util_layers import MLP
 
 class SpectraEnc(nn.Module):
     def __init__(self, 
@@ -196,5 +197,131 @@ class SpectraVAE(VAE):
             pz = self.pz(*self.pz_params)
             zs = pz.rsample(torch.Size([N]))
             px_z = self.px_z(*self.dec(wavelength, phase, zs, mask))
+            data = px_z.mean
+        return data
+
+
+
+
+class BrightSpectraVAE(VAE):
+    def __init__(self, spectra_length = 982,
+                latent_len = 4,
+                latent_dim = 2,
+                model_dim = 32, 
+                num_heads = 4, 
+                ff_dim = 32, 
+                num_layers = 4,
+                dropout = 0.1,
+                selfattn = False,
+                beta = 1.,
+                prior = dist.Laplace,
+                likelihood = dist.Laplace,
+                posterior = dist.Laplace):
+        assert latent_len > 1, "Need at least one token for overall brightness"
+        super(BrightSpectraVAE, self).__init__(
+            prior,  # prior
+            likelihood,  # likelihood
+            posterior,  # posterior
+            SpectraEnc(latent_len,
+                    latent_dim,
+                    model_dim, 
+                    num_heads, 
+                    num_layers,
+                    ff_dim, 
+                    dropout,
+                    selfattn
+                    ),
+            SpectraDec(spectra_length,
+                    latent_dim,
+                    model_dim, 
+                    num_heads, 
+                    ff_dim, 
+                    num_layers,
+                    dropout),
+            params = [
+                    spectra_length,
+                    latent_len,
+                    latent_dim,
+                    model_dim, 
+                    num_heads, 
+                    num_layers,
+                    ff_dim, 
+                    dropout,
+                    selfattn
+                    ]
+        )
+        
+        self._pz_params = nn.ParameterList([
+            nn.Parameter(torch.zeros(latent_len, latent_dim), requires_grad=False),  # mu
+            nn.Parameter(torch.ones(latent_len, latent_dim), requires_grad=False)  # logvar
+        ])
+        self.llik_scaling = 1./beta
+        self.modelName = 'spectrum'
+        self.spectra_length = spectra_length
+        self.latent_len = latent_len
+        self.latent_dim = latent_dim
+        self.brightnessfc = MLP(latent_dim, 1, [model_dim])
+    
+    def forward(self, x, K = 1):
+        flux, wavelength, phase, mask = x
+        self._qz_x_params = self.enc(flux, wavelength, phase, mask)
+        qz_x = self.qz_x(*self._qz_x_params)
+        zs = qz_x.rsample(torch.Size([K]))
+        #breakpoint()
+        '''
+        px_z_loc, px_z_scale = self.dec(wavelength.unsqueeze(0).expand(K, -1, -1).view(-1, wavelength.shape[-1]), 
+                                        phase.unsqueeze(0).expand(K, -1).view(-1), 
+                                        zs.view(-1, zs.shape[-2], zs.shape[-1]), 
+                                        mask.unsqueeze(0).expand(K, -1, -1).view(-1, mask.shape[-1]))
+        px_z_loc = px_z_loc.view(K, -1, flux.shape[-1])
+        px_z_scale = px_z_scale.view(K, -1, flux.shape[-1])
+        px_z = self.px_z(px_z_loc, px_z_scale)
+        '''
+        px_z = self.decode(zs, x)
+        #breakpoint()
+        return qz_x, px_z, zs
+    
+    def reconstruct(self, x, K = 1):
+        flux, wavelength, phase, mask = x
+        self.eval()
+        with torch.no_grad():
+            qz_x = self.qz_x(*self.enc(flux, wavelength, phase, mask))
+            zs = qz_x.rsample([K])  # no dim expansion
+            px_z = self.decode(zs, x)
+            recon = px_z.mean
+        return recon
+    
+    def encode(self, x, mean = True):
+        flux, time, band, mask = x
+        self.eval()
+        with torch.no_grad():
+            qz_x = self.qz_x(*self.enc(flux, time, band, mask))
+        if mean:
+            return qz_x.mean
+        return qz_x
+    
+    def decode(self, zs, x):
+        _, wavelength, phase, mask = x 
+        K = zs.shape[0]
+        brightness = zs[:, :,0, :]
+        brightness = self.brightnessfc(brightness)
+        zs = zs[:, :, 1:, :]
+        px_z_loc, px_z_scale = self.dec(wavelength.unsqueeze(0).expand(K, -1, -1).reshape(-1, wavelength.shape[-1]), 
+                                        phase.unsqueeze(0).expand(K, -1).reshape(-1), 
+                                        zs.reshape(-1, zs.shape[-2], zs.shape[-1]), 
+                                        mask.unsqueeze(0).expand(K, -1, -1).reshape(-1, mask.shape[-1]))
+        px_z_loc = px_z_loc.reshape(K, -1, self.spectra_length)
+        px_z_loc = px_z_loc - px_z_loc.mean(axis = 2)[:, :, None] + brightness
+        px_z_scale = px_z_scale.reshape(K, -1, self.spectra_length)
+
+        return self.px_z(px_z_loc, px_z_scale)
+    
+    def generate(self, N, x):
+        self.eval()
+        _, wavelength, phase, mask = x
+        with torch.no_grad():
+            pz = self.pz(*self.pz_params)
+            zs = pz.rsample(torch.Size([N]))
+            px_z = self.decode(zs, x)
             data = px_z.mean
         return data
